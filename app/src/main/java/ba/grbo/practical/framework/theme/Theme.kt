ec6295b4785d2dc3fa77be6d88cbb1614f4396de
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-11-20 07:42:27 +0100

    Init commit

---
+++ app/src/main/java/ba/grbo/practical/framework/theme/Theme.kt
@@ -0,0 +1,32 @@
+package ba.grbo.practical.framework.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun PracticalTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
---
+++ app/src/main/java/ba/grbo/practical/presentation/PracticalActivity.kt
@@ -0,0 +1,14 @@
+package ba.grbo.practical.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class PracticalActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}

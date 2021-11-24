331ee2df69b6537ef9310b5654a62fe799a389ec
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2021-11-24 18:59:53 +0100

    Do the algorithm assignment

---
+++ app/src/main/java/ba/grbo/algorithm/Algorithm.kt
@@ -0,0 +1,35 @@
+package ba.grbo.algorithm
+
+// Note: Check out the automated tests in AlgorithmTest.kt
+fun getPair(pairs: Array<Pair>): Pair {
+    require(pairs.isNotEmpty()) { MSG_PAIRS_EMPTY }
+    require(pairs.map(Pair::value).all { value -> value >= 0 }) { MSG_VALUES_NEGATIVE }
+
+    val ids = pairs.map(Pair::id)
+    require(ids.size == ids.toSet().size) { MSG_IDS_NOT_UNIQUE }
+
+    val value = pairs
+        .groupBy(Pair::value)
+        .firstNotNullOfOrNull { (value, pairs) -> if (pairs.size > 1) value else null }
+    requireNotNull(value) { MSG_VALUES_UNIQUE }
+
+    return Pair(
+        id = ids.maxOf { it } + 1,
+        value = getValidValue(value, pairs.map(Pair::value))
+    )
+}
+
+private fun getValidValue(nonUniqueValue: Int, values: List<Int>): Int {
+    require(nonUniqueValue in values) { MSG_VALUE_NOT_PRESENT}
+    var value = nonUniqueValue + 1
+    while (value in values) value++
+    return value
+}
+
+data class Pair(val id: Int, val value: Int)
+
+const val MSG_PAIRS_EMPTY = "pairs not allowed to be empty"
+const val MSG_VALUES_NEGATIVE = "values should be positive"
+const val MSG_IDS_NOT_UNIQUE = "ids need to be unique"
+const val MSG_VALUES_UNIQUE = "no value appears twice"
+const val MSG_VALUE_NOT_PRESENT = "nonUniqueValue should be present in values"
---
+++ app/src/test/java/ba/grbo/algorithm/AlgorithmTest.kt
@@ -0,0 +1,192 @@
+package ba.grbo.algorithm
+
+import org.junit.Assert
+import org.junit.Test
+
+
+internal class AlgorithmTest {
+    @Test
+    fun getPairSeriesOfTests() {
+        // Given
+        val arrayOfPairs = arrayOf(
+            arrayOf(
+                Pair(id = 1, value = 3),
+                Pair(id = 2, value = 7),
+                Pair(id = 3, value = 3),
+                Pair(id = 4, value = 1),
+                Pair(id = 5, value = 4)
+            ),
+            arrayOf(
+                Pair(id = 1, value = 1),
+                Pair(id = 2, value = 1),
+                Pair(id = 3, value = 4),
+                Pair(id = 4, value = 7),
+                Pair(id = 5, value = 2),
+                Pair(id = 6, value = 8),
+                Pair(id = 7, value = 12),
+                Pair(id = 8, value = 9),
+                Pair(id = 9, value = 6)
+            ),
+            arrayOf(
+                Pair(id = 1, value = 1),
+                Pair(id = 2, value = 1),
+                Pair(id = 3, value = 1),
+            ),
+            arrayOf(
+                Pair(id = 1, value = 1),
+                Pair(id = 2, value = 1),
+                Pair(id = 3, value = 2),
+                Pair(id = 4, value = 2),
+                Pair(id = 5, value = 3),
+                Pair(id = 6, value = 3),
+                Pair(id = 7, value = 3),
+                Pair(id = 8, value = 4),
+                Pair(id = 9, value = 5),
+                Pair(id = 10, value = 6),
+                Pair(id = 11, value = 6),
+            ),
+            arrayOf(
+                Pair(id = 1, value = 1),
+                Pair(id = 2, value = 1),
+                Pair(id = 3, value = 3),
+                Pair(id = 4, value = 5),
+                Pair(id = 5, value = 7),
+                Pair(id = 6, value = 12),
+                Pair(id = 7, value = 6),
+                Pair(id = 8, value = 7),
+                Pair(id = 9, value = 2),
+                Pair(id = 10, value = 6),
+                Pair(id = 11, value = 4),
+                Pair(id = 12, value = 9),
+                Pair(id = 13, value = 11),
+            ),
+            arrayOf(
+                Pair(id = 1, value = 1),
+                Pair(id = 2, value = 1),
+                Pair(id = 3, value = 1),
+                Pair(id = 4, value = 1)
+            ),
+            arrayOf(
+                Pair(id = 1, value = 4),
+                Pair(id = 2, value = 3),
+                Pair(id = 3, value = 2),
+                Pair(id = 4, value = 1),
+                Pair(id = 5, value = 1),
+                Pair(id = 6, value = 4)
+            ),
+            arrayOf(
+                Pair(id = 1, value = 0),
+                Pair(id = 2, value = 1),
+                Pair(id = 3, value = 0),
+                Pair(id = 4, value = 1),
+                Pair(id = 5, value = 0),
+                Pair(id = 6, value = 1),
+                Pair(id = 7, value = 0),
+                Pair(id = 8, value = 1),
+                Pair(id = 9, value = 0),
+                Pair(id = 10, value = 1),
+            ),
+            arrayOf(
+                Pair(id = 1, value = 7),
+                Pair(id = 2, value = 7),
+                Pair(id = 3, value = 2),
+            ),
+            arrayOf(
+                Pair(id = 1, value = 3),
+                Pair(id = 2, value = 3),
+            ),
+            arrayOf(
+                Pair(id = 1, value = 0),
+                Pair(id = 2, value = 10),
+                Pair(id = 3, value = 20),
+                Pair(id = 4, value = 30),
+                Pair(id = 5, value = 40),
+                Pair(id = 6, value = 10),
+                Pair(id = 7, value = 60),
+            )
+        )
+
+        val solutions = listOf(
+            Pair(id = 6, value = 5),
+            Pair(id = 10, value = 3),
+            Pair(id = 4, value = 2),
+            Pair(id = 12, value = 7),
+            Pair(id = 14, value = 8),
+            Pair(id = 5, value = 2),
+            Pair(id = 7, value = 5),
+            Pair(id = 11, value = 2),
+            Pair(id = 4, value = 8),
+            Pair(id = 3, value = 4),
+            Pair(id = 8, value = 11)
+        )
+
+        // When
+        val computedPairs = arrayOfPairs.map { pairs -> getPair(pairs) }
+
+        // Then
+        Assert.assertEquals(solutions, computedPairs)
+    }
+
+    @Test
+    fun getPairGivenEmptyArrayThrowIllegalArgumentException() {
+        // Given
+        val pairs = arrayOf<Pair>()
+
+        // When && Then
+        val exception = Assert.assertThrows(IllegalArgumentException::class.java) { getPair(pairs)}
+        Assert.assertEquals(MSG_PAIRS_EMPTY, exception.message)
+    }
+
+    @Test
+    fun getPairGivenArrayWithNegativeValuesThrowIllegalArgumentException() {
+        // Given
+        val pairs = arrayOf(
+            Pair(id = 1, value = 15),
+            Pair(id = 2, value = 11),
+            Pair(id = 3, value = 4),
+            Pair(id = 4, value = 13),
+            Pair(id = 5, value = 9),
+            Pair(id = 6, value = -11),
+        )
+
+        // When && Then
+        val exception = Assert.assertThrows(IllegalArgumentException::class.java) { getPair(pairs)}
+        Assert.assertEquals(MSG_VALUES_NEGATIVE, exception.message)
+    }
+
+    @Test
+    fun getPairGivenArrayWithNonUniqueIdsThrowIllegalArgumentException() {
+        // Given
+        val pairs = arrayOf(
+            Pair(id = 1, value = 4),
+            Pair(id = 2, value = 7),
+            Pair(id = 3, value = 2),
+            Pair(id = 1, value = 2),
+            Pair(id = 4, value = 8)
+        )
+
+        // When && Then
+        val exception = Assert.assertThrows(IllegalArgumentException::class.java) { getPair(pairs)}
+        Assert.assertEquals(MSG_IDS_NOT_UNIQUE, exception.message)
+    }
+
+    @Test
+    fun getPairGivenArrayWithUniqueValuesThrowIllegalArgumentException() {
+        // Given
+        val pairs = arrayOf(
+            Pair(id = 1, value = 1),
+            Pair(id = 2, value = 3),
+            Pair(id = 3, value = 2),
+            Pair(id = 4, value = 7),
+            Pair(id = 5, value = 5),
+            Pair(id = 6, value = 8),
+            Pair(id = 7, value = 12),
+            Pair(id = 8, value = 9),
+            Pair(id = 9, value = 6)
+        )
+
+        // When && Then
+        val exception = Assert.assertThrows(IllegalArgumentException::class.java) { getPair(pairs)}
+        Assert.assertEquals(MSG_VALUES_UNIQUE, exception.message)
+    }
+}
